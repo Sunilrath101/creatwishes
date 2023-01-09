607a4c7a2c7883e13354060f4d2e70913ccfbee1
Sunilrath101 <sunilrath.ssr@gmail.comgit config --global user.email sunilrath.ssr@gmail.comgit config --global user.email sunilrath.ssr@gmail.com>
2023-01-10 02:17:13 +0530

    server create

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const dbConnected = require("./config/db");
 const wishesRouter = require("./routes/wishes.routes");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/", wishesRouter);
 
-app.listen(PORT, async (err) => {
-  await dbConnected();
-  if (err) {
-    return console.error(err);
-  }
-  console.log(`Server listening on port ${PORT}`);
+dbConnected().then(() => {
+  app.listen(PORT, async (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
